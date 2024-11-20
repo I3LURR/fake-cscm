2f1e9e7154d9c322f9fe3361d81116aa930dcdcf
tmolear <tmolear@svsu.edu>
2024-11-19 22:57:51 -0500

    Initial commit

---
+++ src/main/java/com/cscm/cscmfcna/CscmfcnaApplication.java
@@ -0,0 +1,13 @@
+package com.cscm.cscmfcna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CscmfcnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CscmfcnaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cscm/cscmfcna/CscmfcnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cscm.cscmfcna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CscmfcnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
